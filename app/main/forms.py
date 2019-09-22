ca1dc4c3a1b493bd72156c9be42549e7969591a3
Joselyne97 <joselynejojo740@gmail.com>
2019-09-22 02:31:37 +0200

    I added the loggin form, registration form, upvote, downvote and comments

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,13 +1,22 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField,ValidationError
+from wtforms import StringField,PasswordField,SubmitField,BooleanField,SubmitField,TextAreaField,RadioField
 from wtforms.validators import Required,Email
+from wtforms import ValidationError
 
-class ReviewForm(FlaskForm):
-
-    title = StringField('Review title',validators=[Required()])
-    review = TextAreaField('Movie review')
+class PitchForm(FlaskForm):
+    title = StringField('title',validators=[Required()])
+    description = TextAreaField("What would you like to pitch?",validators=[Required()])
+    category = RadioField('Label', choices=[ ('pickup','pickup'), ('interview','interview'),('promotion','promotion'),('product','product')],validators=[Required()])
     submit = SubmitField('Submit')
 
-class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Tell us about you.',validators = [Required()])
-    submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+	description = TextAreaField('Add comment',validators=[Required()])
+	submit = SubmitField()
+
+class UpvoteForm(FlaskForm):
+	submit = SubmitField()
+
+
+class Downvote(FlaskForm):
+	submit = SubmitField()
