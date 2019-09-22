f98dedbfcc4bf6ed3ba0dc3e5cc4311c5e7879d4
Joselyne97 <joselynejojo740@gmail.com>
2019-09-22 02:45:42 +0200

    I added the pitch details, comment, registration form, upvote ad downvote info

--- app/__init__.py
+++ app/__init__.py
@@ -3,20 +3,18 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
 from flask_simplemde import SimpleMDE
 
-simple = SimpleMDE()
+
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
-photos = UploadSet('photos',IMAGES)
 mail = Mail()
-
+simple = SimpleMDE()
 def create_app(config_name):
 
     app = Flask(__name__)
@@ -25,7 +23,6 @@ def create_app(config_name):
     # Creating the app configurations
 
     app.config.from_object(config_options[config_name])
-    config_options[config_name].init_app(app)
 
     # Initializing flask extensions
     bootstrap.init_app(app)
@@ -40,12 +37,5 @@ def create_app(config_name):
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
-
-    # setting config
-    # from .requests import configure_request
-    # configure_request(app)
-
-
-
 
     return app
