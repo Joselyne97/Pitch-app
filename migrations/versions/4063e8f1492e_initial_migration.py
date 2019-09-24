ef5296067fd8364bc6a0c34c64bd27b1d82f986d
Joselyne97 <joselynejojo740@gmail.com>
2019-09-24 19:38:30 +0200

    deployment to heroku

--- migrations/versions/4063e8f1492e_initial_migration.py
+++ migrations/versions/4063e8f1492e_initial_migration.py
@@ -1,8 +1,8 @@
 """Initial Migration
 
-Revision ID: ce61b8368f4f
+Revision ID: 4063e8f1492e
 Revises: 
-Create Date: 2019-09-24 17:48:24.573518
+Create Date: 2019-09-24 19:35:57.163815
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'ce61b8368f4f'
+revision = '4063e8f1492e'
 down_revision = None
 branch_labels = None
 depends_on = None
